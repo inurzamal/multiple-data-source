8568bedc7f86b0cba5b0a8f92d73c65c59da971e
Nurzamal Islam <n.zaman98@gmail.com>
2025-02-23 00:39:13 +0530

    configuration enhancement

--- src/main/java/com/nur/configuration/MongoDBConfig.java
+++ src/main/java/com/nur/configuration/MongoDBConfig.java
@@ -2,6 +2,7 @@ package com.nur.configuration;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
@@ -13,9 +14,12 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @EnableMongoRepositories(basePackages = "com.nur.amongodb.repository")
 public class MongoDBConfig {
 
+    @Value("${spring.data.mongodb.uri}")
+    private String mongoUri;
+
     @Bean
     public MongoClient mongoClient() {
-        return MongoClients.create("mongodb://localhost:27017");
+        return MongoClients.create(mongoUri);
     }
 
     @Bean
--- src/main/java/com/nur/configuration/MySQLConfig.java
+++ src/main/java/com/nur/configuration/MySQLConfig.java
@@ -2,7 +2,6 @@ package com.nur.configuration;
 
 import jakarta.persistence.EntityManagerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +14,8 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @EnableTransactionManagement
@@ -27,19 +28,29 @@ public class MySQLConfig {
 
     @Primary
     @Bean(name = "mysqlDataSource")
-    @ConfigurationProperties(prefix = "spring.datasource.hikari")
     public DataSource dataSource() {
-        return DataSourceBuilder.create().build();
+        return DataSourceBuilder.create()
+                .driverClassName("com.mysql.cj.jdbc.Driver")
+                .url("jdbc:mysql://localhost:3306/adwp_work1")
+                .username("root")
+                .password("root")
+                .build();
     }
 
     @Primary
     @Bean(name = "mysqlEntityManagerFactory")
     public LocalContainerEntityManagerFactoryBean entityManagerFactory(
             EntityManagerFactoryBuilder builder, @Qualifier("mysqlDataSource") DataSource dataSource) {
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("hibernate.hbm2ddl.auto", "update");
+        properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
+        properties.put("hibernate.show_sql", true);
+
         return builder
                 .dataSource(dataSource)
                 .packages("com.nur.entity") // MySQL entities
                 .persistenceUnit("mysql")
+                .properties(properties)
                 .build();
     }
 
